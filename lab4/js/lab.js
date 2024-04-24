0f97c44b7cfd275dfadadfc37bfcb5cd4ebadd40
Maddy Xiong <madixi03@gmail.com>
2024-04-23 23:22:49 -0700

    lab 4 add pseudocode
    added our psuedocode comments to js file

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Go Fish
+// Author: Maddz Xiong
+// Date:04/23/24
 
 // Constants
 
@@ -15,7 +15,24 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  //Deal each player three cards
+  //User goes first.
+  //Ask user for input on which card in your deck to ask from computer.
+  //Run a loop checking computer’s deck for the correct input
+    //If correct cpu gives user the card 
+      //CPU draws card
+    //If incorrect CPU says “go fish”
+      //Player draws card
+  //CPU chooses random card in deck
+  //Run a loop checking user’s deck for the correct input
+    //If correct user gives CPU the card
+      //User draws card
+    //If incorrect User says “go fish”
+      //CPU draws card
+  //Repeat until one player has matched 3 of the same cards
+  //First to three matches wins
 }
 
 // let's get this party started
 main();
+
