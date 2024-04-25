ff14bbb01a61f845d72252b927647efba492d703
Maddy Xiong <madixi03@gmail.com>
2024-04-24 20:24:51 -0700

    Lab 5 updates
    some css, js, and index.html updates for lab 5 task 1-3

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5
+// Author: Madison Xiong
+// Date: 04/24/24
 
-// Constants
+// Variables
+let make = "Honda";
+let model = "Civic";
+let color = "Silver";
+let year = 1991;
+let ownIt = "True";
 
-// Functions
+// Calculations
+let age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Outputs
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years");
