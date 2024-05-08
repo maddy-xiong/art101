9d0ad6b57ab807c8aa202ebe3ab2f5299bb60243
Maddy Xiong <madixi03@gmail.com>
2024-05-08 12:34:18 -0700

    lab 9 TASK X
    simplified adding buttons and selectors/event listeners

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,22 +4,33 @@
 
 // Constants
 
-// Adding buttons to sections
+/* TASK 2 Adding buttons to sections
 $("#challenge").append("<button id='button-challenge'>Click Me</button>");
 $("#problems").append("<button id='button-problems'>Make Special</button>");
 $("#results").append("<button id='button-results'>Make Special</button>");
+*/
 
-//Functions
+// TASK X
+$(".minor-section").append("<button id='special-button'>Click Me</button>");
+
+// Functions
 function changeClass(){
   console.log("Click");
   $("#challenge").toggleClass("special");
 }
 
 // Event Listeners
+/* TASK 2
 $("#button-challenge").click(changeClass);
 $("#button-problems").click(function(){
   $("#problems").toggleClass("special");
 });
 $("#button-results").click(function(){
   $("#results").toggleClass("minor-section");
+});
+*/
+
+// TASK X
+$(".minor-section button").click(function(){
+  $(this).parent().toggleClass("special-section");
 });
