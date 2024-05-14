f4ec49b033073063748b2f5ddea03d1835913856
Maddy Xiong <madixi03@gmail.com>
2024-05-14 16:21:53 -0700

    lab 11 Task X
    task x updates

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -10,9 +10,21 @@ function sortString(inputString){
   return inputString.split('').sort().join('');
 }
 
-$("#submit").click(function(){
+$("#submit-name").click(function(){
   console.log("click");
   const userName = $("#user-name").val();
   const sortedUser = sortString(userName);
-  $("#output").html("<div class='text'><p>" + sortedUser + "</p></div>");
+  $("#output").html("<div id='text'><p>" + sortedUser + "</p></div>");
+});
+
+// TASK X
+$("#submit-age").click(function(){
+  console.log("click");
+  const userAge = $("#user-age").val();
+  $("#text").append("<p>" + userAge + "</p>");
+});
+
+$("#color-change").click(function(){
+  console.log("click");
+  $("#text").toggleClass("new-color");
 });
