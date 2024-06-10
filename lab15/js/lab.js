ec5a6b072cf7a94c7293e0d0390447b1390d735a
Maddy Xiong <madixi03@gmail.com>
2024-06-09 21:40:56 -0700

    lab 15 update
    GOD I HATE CS

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,7 +3,28 @@
 // Date: 06/06/24
 
 // Constants
+const URL = "https://api.chucknorris.io/jokes/random";
+
+const ajaxObj = {
+    url: URL,
+    //data: {},
+    type: "GET",
+    dataType: "json",
+    success: ajaxSuccess,
+    error: ajaxFail
+}
 
 // Functions
+function ajaxSuccess(data) {
+    console.log("Data: ", data);
+    const chuck = data.value;
+    $("#output").html(chuck);
+}
 
+function ajaxFail(request, error) {
+    console.log("Oops: ", request, error);
+}
 
+$("#activate").click(function(){
+    $.ajax(ajaxObj);
+})
